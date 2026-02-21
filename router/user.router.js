f148dbbf919631120a04d25fabef63db9d212067
0ARTKUNG0 <136410445+0ARTKUNG0@users.noreply.github.com>
2026-02-21 21:29:54 +0700

    feat: Implement user authentication routes and JWT verification middleware,.

--- router/user.router.js
+++ router/user.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controllers");
+const { verifyToken } = require("../middleware/auth.middleware");
 
 router.post("/signup", userController.SignUp);
 router.post("/signin", userController.SignIn);
 router.post("/signout", userController.signOut);
-router.post("/update-username", userController.updateUsername);
+router.post("/update-username", verifyToken, userController.updateUsername);
 
 module.exports = router;
